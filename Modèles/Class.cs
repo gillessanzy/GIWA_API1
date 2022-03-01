c6624e9cc41b0eebe8fbe12dc23d8a0c736786a5
SANZOUANGO <gilles-madelin.sanzouango@groupe-esigelec.org>
2022-03-01 17:23:18 +0100

    Ajout des classes Pays et Population

---
+++ Modèles/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.Modèles
+{
+    public class Test
+    {
+        public int id { get; set; }
+    }
+}
---
+++ Modèles/Pays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.Modèles
+{
+    public class Pays
+    { public int Id { get; set; }
+      public  List<Population> LesPopulations { get; set; }
+    }
+}
