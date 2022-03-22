84fdaa7ebd4abffe3482deaeb42c2367fdf88176
gillessanzy <100214473+gillessanzy@users.noreply.github.com>
2022-03-22 16:05:02 +0100

    Pull updates from master (#5)
    * Gilles (#1)
    
    * Mise à jour Pays.cs
    
    Nouvelle classe pays.cs avec un clé étrangère référençant Continent
    
    * Mise à jour Population.cs
    
    Mise à jour de la classe Population.cs avec ajout d'un attribut id_pays référençant la table Pays
    
    * Walter songo (#2)
    
    * Supression de class.cs
    
    La classe est inutile
    
    * Création de la classe continent.cs
    
    * Walter songo (#3)
    
    * Supression de class.cs
    
    La classe est inutile
    
    * Création de la classe continent.cs
    
    * Ajout des classes de transfert de données
    
    Classes de transfert de données (DTO) pour les pays et les continents
    
    * Gilles (#4)
    
    * Mise à jour Pays.cs
    
    Nouvelle classe pays.cs avec un clé étrangère référençant Continent
    
    * Mise à jour Population.cs
    
    Mise à jour de la classe Population.cs avec ajout d'un attribut id_pays référençant la table Pays
    
    Co-authored-by: STMW <60248296+STMW@users.noreply.github.com>

---
+++ DTO/ContinentsPopulationAnneeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.DTO
+{
+    public class ContinentsPopulationAnneeDTO
+    {
+        public int Id_continent { get; set; }
+        public string nom_continent { get; set; }
+        public int annee { get; set; }
+        public int population { get; set; }
+    }
+}
