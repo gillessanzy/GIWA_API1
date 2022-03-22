5423df03c08a71f205cee390f771ee7c98b8a2f7
STMW <60248296+STMW@users.noreply.github.com>
2022-03-22 15:35:42 +0100

    Walter songo (#2)
    * Supression de class.cs
    
    La classe est inutile
    
    * Création de la classe continent.cs

---
+++ Modèles/Continent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.Modèles
+{
+    public class Continent
+    {
+        public int Id { get; set; }
+        public string nom { get; set; }
+    }
+}
