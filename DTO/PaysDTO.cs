411c7a6746daa64a3ee819c74013cf56edb4f65e
STMW <60248296+STMW@users.noreply.github.com>
2022-03-22 15:51:24 +0100

    Ajout des classes de transfert de données
    Classes de transfert de données (DTO) pour les pays et les continents

---
+++ DTO/PaysDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.DTO
+{
+    public class PaysDTO
+    {
+        public int Id { get; set; }
+        public string nom_pays { get; set; } = "";
+        public int Id_continent { get; set; }
+        public string nom_continent { get; set; }
+    }
+}
