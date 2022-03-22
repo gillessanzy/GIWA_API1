96f9febb493c375f7f51e5dc2b2cceca6c5475c7
gillessanzy <100214473+gillessanzy@users.noreply.github.com>
2022-03-22 14:49:41 +0100

    Gilles (#1)
    * Mise à jour Pays.cs
    
    Nouvelle classe pays.cs avec un clé étrangère référençant Continent
    
    * Mise à jour Population.cs
    
    Mise à jour de la classe Population.cs avec ajout d'un attribut id_pays référençant la table Pays

--- Modèles/Pays.cs
+++ Modèles/Pays.cs
@@ -1,12 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace GIWA_API1.Modèles
 {
     public class Pays
-    { public int Id { get; set; }
-      public  List<Population> LesPopulations { get; set; }
+    { 
+        public int Id { get; set; }
+        public string nom_pays { get; set; } = "";
+        public int Id_continent { get; set; }
+        [ForeignKey("Id_continent")]
+        public Continent continent { get; set; }
     }
 }
