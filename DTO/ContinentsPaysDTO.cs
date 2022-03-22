136c3e8ef27e3437a5caaf7be26cd01e95a7bba8
STMW <60248296+STMW@users.noreply.github.com>
2022-03-22 15:55:42 +0100

    Walter songo (#3)
    * Supression de class.cs
    
    La classe est inutile
    
    * Création de la classe continent.cs
    
    * Ajout des classes de transfert de données
    
    Classes de transfert de données (DTO) pour les pays et les continents

---
+++ DTO/ContinentsPaysDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIWA_API1.DTO
+{
+    public class ContinentsPaysDTO
+    {
+        public int Id_continent { get; set; }
+        public string nom_continent { get; set; }
+        public int Id_pays { get; set; }
+        public string nom_pays { get; set; }
+
+    }
+}
