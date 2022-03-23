9901dd7616ceaefb5a5a6f9b703f1942482b63b1
STMW <60248296+STMW@users.noreply.github.com>
2022-03-23 05:59:41 +0100

    Ajout de packages pour Framework 3.0
    Pour les problèmes de build avec dotnet 3.0

---
+++ Data/GIWA_API1Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GIWA_API1.Modèles;
+
+namespace GIWA_API1.Data
+{
+    public class GIWA_API1Context : DbContext
+    {
+        public GIWA_API1Context (DbContextOptions<GIWA_API1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GIWA_API1.Modèles.Continent> Continent { get; set; }
+
+        public DbSet<GIWA_API1.Modèles.Pays> Pays { get; set; }
+
+        public DbSet<GIWA_API1.Modèles.Population> Population { get; set; }
+    }
+}
